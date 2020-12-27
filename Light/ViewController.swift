9efad5fff0ad1b2f1c5780a0aaa97984fa5bcab7
Elliott Barnes <enbarnes@mun.ca>
2020-12-27 18:16:24 -0330

    finished Light tutorial

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    // remove fileprivate if function is called outside of file
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
 }
 
